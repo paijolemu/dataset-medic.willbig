4da52dd1213c76d34d2bb469f34ae2ab8081929d
paijo lemu <adnanprayogo03@gmail.com>
2025-07-23 00:08:42 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const toggleBtn = document.getElementById('toggle-btn');
     const sidebar = document.getElementById('sidebar');
+    const body = document.body;
 
     // Fungsi untuk mengubah status sidebar
     function toggleSidebar() {
-        // Kita hanya perlu mengubah class pada sidebar itu sendiri
         sidebar.classList.toggle('collapsed');
+        body.classList.toggle('overlay-active'); // Tambah/hapus class di body
     }
 
     // Tambahkan event listener ke tombol
