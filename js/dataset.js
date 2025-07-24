772e7c27d6d0475d70e96833621523f26c34d563
paijo lemu <adnanprayogo03@gmail.com>
2025-07-24 12:48:08 +0700

    Update dataset.js

--- js/dataset.js
+++ js/dataset.js
@@ -20,7 +20,7 @@ const allDatasets = {
     kidneyStone: {
         images: [
             // GANTI DENGAN NAMA FILE ASLI ANDA UNTUK BATU GINJAL
-            "https://github.com/paijolemu/dataset-medic.willbig/blob/main/datasets/batu-ginjal-ct-v1/1-3-46-670589-33-1-63700780314615924400001-4656503585389726474_png_jpg.rf.d4e842e4f0d5ca66addcadd37aa3e49d.jpg?raw=true",
+            1-3-46-670589-33-1-63700780314615924400001-4656503585389726474_png_jpg.rf.d4e842e4f0d5ca66addcadd37aa3e49d.jpg?,
             "https://github.com/paijolemu/dataset-medic.willbig/blob/main/datasets/batu-ginjal-ct-v1/1-3-46-670589-33-1-63703718086101119100001-4867318355234768205_png_jpg.rf.048e7b96df9b04ced19bff02cf2fb7af.jpg?raw=true",
             "https://github.com/paijolemu/dataset-medic.willbig/blob/main/datasets/batu-ginjal-ct-v1/1-3-46-670589-33-1-63703718086140121300001-4751980723480797909_png_jpg.rf.4613088faec825a93a3e03e97ce46323.jpg?raw=true",
             "https://github.com/paijolemu/dataset-medic.willbig/blob/main/datasets/batu-ginjal-ct-v1/1-3-46-670589-33-1-63703718086162122600001-5663486194805675763_png_jpg.rf.a4a19de71b774b45786c63f55a9a9016.jpg?raw=true",
